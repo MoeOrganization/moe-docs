9b63d095d25e676cde9637d07dcaac85d4d4eb75
Kartik Thakore <thakore.kartik@gmail.com>
2013-01-25 15:05:37 -0500

    Making git commit happen automatically

--- make.sh
+++ make.sh
@@ -1,3 +1,11 @@
 cd ../moe;
 ack -f --type-set scala=.scala --scala *  | xargs scaladoc -d ../moe-docs/
+commit=$(git log -1 --pretty=format:'%h');
+cd ../moe-docs/
+
+commit="Doing doc for $commit"
+echo $commit
+
+git add . 
+git commit -m "$commit"
 
